3dfdf4146fb7766232a39be718fe29b896a88885
A Pawan <alugolupawan.com>
2025-02-23 00:03:19 +0530

    Initial commit

--- OS/boot.asm
+++ OS/boot.asm
@@ -1,13 +1,13 @@
 [BITS 16]
-[ORG 0x7C00]   ; Boot sector memory location
+[ORG 0x7C00]
 
-mov ah, 0x0E  ; BIOS teletype output
+mov ah, 0x0E
 mov al, 'H'
-int 0x10      ; Print 'H'
+int 0x10
 mov al, 'i'
-int 0x10      ; Print 'i'
+int 0x10
 
-jmp $         ; Infinite loop to stop execution
+jmp $
 
-times 510-($-$$) db 0  ; Fill with zeroes until 510 bytes
-dw 0xAA55      ; Boot signature
+times 510-($-$$) db 0
+dw 0xAA55
