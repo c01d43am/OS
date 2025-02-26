e4c61bfd2f60501a5c5172207277cae335eefda9
A Pawan <alugolupawan.com>
2025-02-26 21:07:39 +0530

    initial commit

--- OS/keyboard.asm
+++ OS/keyboard.asm
@@ -1,15 +1,15 @@
-; Comment out or remove this section
-; [GLOBAL keyboard_handler]
-; keyboard_handler:
-;    push rax
-;    push rbx
-;    push rcx
-;    push rdx
-;
-;    ; Your keyboard logic here
-;
-;    pop rdx
-;    pop rcx
-;    pop rbx
-;    pop rax
-;    iretq
+Comment out or remove this section
+[GLOBAL keyboard_handler]
+keyboard_handler:
+   push rax
+   push rbx
+   push rcx
+   push rdx
+
+   ; Your keyboard logic here
+
+   pop rdx
+   pop rcx
+   pop rbx
+   pop rax
+   iretq
