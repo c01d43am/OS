4d7b09439e22afcfa6748d17a7b510d365ecfd42
A Pawan <alugolupawan.com>
2025-02-26 22:00:03 +0530

    Initial commit

--- OS/kernel_entry.asm
+++ OS/kernel_entry.asm
@@ -5,29 +5,23 @@ extern kernel_main   ; Tell assembler kernel_main() exists in C
 
 section .text
 _start:
-    call kernel_main  ; Call the kernel main function
-    hlt               ; Halt the CPU
+    cli                 ; Disable interrupts
+    mov esp, stack_top  ; Set up the stack
+    call kernel_main    ; Call the main kernel function
+    hlt                 ; Halt CPU if kernel_main returns
 
 [BITS 32]
 [GLOBAL multiboot_header]
 [EXTERN kernel_main]
 
-SECTION .multiboot
+section .multiboot
 align 4
 multiboot_header:
     dd 0x1BADB002  ; Magic number
     dd 0x00        ; Flags
     dd -(0x1BADB002 + 0x00) ; Checksum (magic + flags + checksum = 0)
 
-SECTION .text
-global _start32
-_start32:
-    ; Load stack
-    mov esp, stack_top
-    call kernel_main  ; Jump to kernel_main in C
-
-    hlt   ; Halt CPU if kernel_main returns
-
-SECTION .bss
-resb 8192  ; 8 KB stack
+section .bss
+align 16
+resb 16384  ; Reserve 16 KB stack space
 stack_top:
